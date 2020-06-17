5573de343587c6bac92ed7b99ccd2463d1ff2b6d
Benjamin Wießneth <b.wiessneth@gmail.com>
2020-06-17 19:17:24 +0200

    Added support for flashing

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from app import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
 
 api_url = 'https://bewi.uber.space/flask_rest_api/api/v0/'
 api_user_endpoint = 'user'
@@ -17,9 +17,9 @@ def index():
 @app.route("/user/<user_id>")
 def user(user_id=None):
 	if user_id:
-		r = requests.get(url=api_url + api_user_endpoint + '/' + user_id)		
+		r = requests.get(url=api_url + api_user_endpoint + '/' + user_id)
 		if r.status_code == 200:
-			d = json.loads(r.text)		
+			d = json.loads(r.text)			
 			return render_template('user_view.html', user=d)
 	else:
 		r = requests.get(url=api_url + api_user_endpoint)
@@ -36,8 +36,8 @@ def user_create():
 		r = requests.post(url=api_url + api_user_endpoint, data=j, headers=headers)
 		return redirect(url_for('user'))
 	else:
-		r = requests.get(url=api_url + api_user_endpoint + '/' + '1')		
-		d = json.loads(r.text)				
+		r = requests.get(url=api_url + api_user_endpoint + '/' + '1')
+		d = json.loads(r.text)
 		print(d)
 		return render_template('user_create.html', user=d)
 
@@ -48,13 +48,13 @@ def user_update(user_id):
 		headers = {'Content-type': 'application/json'}
 		j = json.dumps(request.form)
 		r = requests.patch(url=api_url + api_user_endpoint + '/' + user_id, data=j, headers=headers)
-		return redirect(url_for('user'))	
+		return redirect(url_for('user'))
 	else:
-		r = requests.get(url=api_url + api_user_endpoint + '/' + user_id)			
-		user = json.loads(r.text)		
+		r = requests.get(url=api_url + api_user_endpoint + '/' + user_id)
+		user = json.loads(r.text)
 		return render_template('user_update.html', user=user)
 
 @app.route("/user/delete/<user_id>")
 def user_delete(user_id):
-	r = requests.delete(url=api_url + api_user_endpoint + '/' + user_id)		
-	return redirect(url_for('user'))	
+	r = requests.delete(url=api_url + api_user_endpoint + '/' + user_id)
+	return redirect(url_for('user'))
